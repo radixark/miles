a806cbbfc7bde7f3f7b30295f29fdcbe25d59a7d
fzyzcjy <ch271828n@outlook.com>
2025-10-09 15:05:30 +0800

    more

--- tests/command_utils.py
+++ tests/command_utils.py
@@ -4,3 +4,13 @@ import subprocess
 def exec_command(cmd: str):
     print(f"EXEC: {cmd}", flush=True)
     subprocess.run(cmd, shell=True, check=True)
+
+
+def convert_checkpoint():
+    exec_command(
+        "source scripts/models/glm4-9B.sh && "
+        "PYTHONPATH=/root/Megatron-LM torchrun --nproc-per-node 8 tools/convert_hf_to_torch_dist.py "
+        "${MODEL_ARGS[@]} "
+        "--hf-checkpoint /root/models/GLM-Z1-9B-0414 "
+        "--save /root/GLM-Z1-9B-0414_torch_dist"
+    )
--- tests/test_quick_start_glm4_9B.py
+++ tests/test_quick_start_glm4_9B.py
@@ -7,6 +7,8 @@ def prepare():
     U.exec_command("hf download --repo-type dataset zhuzilin/dapo-math-17k --local-dir /root/datasets/dapo-math-17k")
     U.exec_command("hf download --repo-type dataset zhuzilin/aime-2024 --local-dir /root/datasets/aime-2024")
 
+    U.exec_command("hf download --repo-type dataset zhuzilin/aime-2024 --local-dir /root/datasets/aime-2024")
+
 
 def execute():
     TODO
