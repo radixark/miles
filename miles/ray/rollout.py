1e259076918b4eb250d6d01148a09a44d7213352
fzyzcjy <ch271828n@outlook.com>
2025-10-10 22:08:27 +0800

    more

--- miles/ray/rollout.py
+++ miles/ray/rollout.py
@@ -353,7 +353,7 @@ def init_rollout_engines(args, pg, all_rollout_engines):
 
 def _allocate_rollout_engine_addr_and_ports_external(args, rollout_engines):
     addr_and_ports = []
-    for rank, _ in range(rollout_engines):
+    for rank, _ in rollout_engines:
         [host, port] = args.rollout_external_engine_addrs[rank].split(":")
         addr_and_ports.append(
             dict(
