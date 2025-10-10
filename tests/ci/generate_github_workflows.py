a1bb62ce7d0cfb860d7d9721204ff91397bbfa23
fzyzcjy <ch271828n@outlook.com>
2025-10-10 15:15:21 +0800

    more

--- tests/ci/generate_github_workflows.py
+++ tests/ci/generate_github_workflows.py
@@ -8,8 +8,10 @@ def main():
     """
     workflows_dir = Path(".github/workflows")
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(workflows_dir)),
-                             variable_start_string="<<",
-                             variable_end_string=">>")
+                             block_start_string='<%',
+                             block_end_string='%>',
+                             variable_start_string='<<',
+                             variable_end_string='>>')
 
     for template_path in workflows_dir.glob("*.yml.j2"):
         template = env.get_template(template_path.name)
@@ -17,7 +19,7 @@ def main():
 
         yaml_path = template_path.with_suffix('')
         with open(yaml_path, "w") as f:
-            f.write("# This file is auto-generated from a template. Do not edit manually.\n\n")
+            f.write("#" * 80 + "\n# This file is auto-generated from a template. Do not edit manually.\n" + "#" * 80 + "\n")
             f.write(content)
 
         print(f"Generated {yaml_path} from {template_path}")
