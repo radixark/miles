13146080f6e546e5f67635206bbe6ac4109bb074
fzyzcjy <ch271828n@outlook.com>
2025-10-12 21:55:58 +0800

    fix group_by

--- miles/utils/iter_utils.py
+++ miles/utils/iter_utils.py
@@ -2,9 +2,9 @@ from collections import defaultdict
 
 
 # details: https://stackoverflow.com/questions/773/how-do-i-use-itertools-groupby
-def group_by(iterable, key):
+def group_by(iterable, key=None):
     """Similar to itertools.groupby, but do not require iterable to be sorted"""
     ret = defaultdict(list)
-    for k in iterable:
-        ret[key(k)].append(k)
+    for item in iterable:
+        ret[key(item) if key is not None else item].append(item)
     return dict(ret)
