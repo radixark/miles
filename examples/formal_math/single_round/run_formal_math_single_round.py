ec118921ca38936a56acb171a067bf3e5a04fdd8
fzyzcjy <ch271828n@outlook.com>
2025-10-15 15:57:03 +0800

    more

--- examples/formal_math/single_round/run_formal_math_single_round.py
+++ examples/formal_math/single_round/run_formal_math_single_round.py
@@ -34,7 +34,7 @@ def execute():
         "--label-key label "
         "--apply-chat-template "
         "--rollout-shuffle "
-        "--rm-type deepscaler "
+        "--custom-rm-path reward_fn.reward_func "
         "--num-rollout 3000 "
         "--rollout-batch-size 32 "
         "--n-samples-per-prompt 8 "
