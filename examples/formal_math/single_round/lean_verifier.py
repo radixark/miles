604daa50932e5cb9ba547584d1a4b254e19738ad
fzyzcjy <ch271828n@outlook.com>
2025-10-15 16:33:07 +0800

    more

--- examples/formal_math/single_round/lean_verifier.py
+++ examples/formal_math/single_round/lean_verifier.py
@@ -1,15 +1,26 @@
-from typing import List
+from typing import List, Callable
 
 from kimina_client import AsyncKiminaClient
 
 
 class LeanVerifier:
     def __init__(self):
-        self._servers = _launch_servers()
-        self._clients = [AsyncKiminaClient(TODO) for server in self._servers]
+        self._servers = _create_servers()
+        self._client_cluster = _KiminaClientCluster(TODO)
 
-    async def check(self):
-        return TODO
+    async def check(self, *args, **kwargs):
+        return await self._client_cluster.check(*args, **kwargs)
+
+
+class _KiminaClientCluster:
+    def __init__(self):
+        self._clients = [AsyncKiminaClient(TODO) for TODO in TODO]
+        self._next_client_index = 0
+
+    async def check(self, *args, **kwargs):
+        client = self._clients[self._next_client_index]
+        self._next_client_index = (self._next_client_index + 1) % len(self._clients)
+        return await client.check(*args, **kwargs)
 
 
 # TODO handle docker stop more gracefully
