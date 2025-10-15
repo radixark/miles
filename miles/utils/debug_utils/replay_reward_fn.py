7b50b3ea55d7a77cb3c6dce6632de6938b46910c
fzyzcjy <ch271828n@outlook.com>
2025-10-15 21:51:03 +0800

    more

--- miles/utils/debug_utils/replay_reward_fn.py
+++ miles/utils/debug_utils/replay_reward_fn.py
@@ -1,15 +1,23 @@
+import asyncio
 from typing import Annotated
 
 import torch
 import typer
 
+from miles.utils.misc import load_function
+
 
 def main(
     rollout_data_path: Annotated[str, typer.Option()],
+    custom_rm_path: Annotated[str, typer.Option()],
 ):
     pack = torch.load(rollout_data_path)
-    samples = pack["samples"]
-    TODO
+    asyncio.run(_main_async(samples=pack["samples"], custom_rm_path=custom_rm_path))
+
+
+async def _main_async(samples, custom_rm_path):
+    rm_function = load_function(custom_rm_path)
+    await asyncio.gather(*[rm_function(None, sample) for sample in samples])
 
 
 if __name__ == '__main__':
