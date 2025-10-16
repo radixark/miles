e507d9f5059dc546c379d59b762a91e33e121d40
fzyzcjy <ch271828n@outlook.com>
2025-10-16 10:12:34 +0800

    more

--- examples/formal_math/single_round/run.py
+++ examples/formal_math/single_round/run.py
@@ -18,8 +18,6 @@ NUM_GPUS = 8
 def prepare():
     U.exec_command("mkdir -p /root/models /root/datasets")
     U.exec_command(f"huggingface-cli download Qwen/{MODEL_NAME} --local-dir /root/models/{MODEL_NAME}")
-    U.hf_download_dataset("zhuzilin/dapo-math-17k")
-    U.hf_download_dataset("zhuzilin/aime-2024")
     U.convert_checkpoint(model_name=MODEL_NAME, model_type=MODEL_TYPE, num_gpus=NUM_GPUS)
 
 
