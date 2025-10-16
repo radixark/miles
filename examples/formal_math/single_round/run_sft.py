953b5e4e0f32924d11a2e74739dc7b52d29b2ef2
fzyzcjy <ch271828n@outlook.com>
2025-10-16 16:50:25 +0800

    more

--- examples/formal_math/single_round/run_sft.py
+++ examples/formal_math/single_round/run_sft.py
@@ -24,6 +24,75 @@ def prepare():
 def execute():
     run_id = f"{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}-{random.randint(0, 1000000)}"
 
+    CKPT_ARGS = (
+        "--hf-checkpoint /root/Qwen3-4B-Base/ "
+        "--ref-load /root/Qwen3-4B-Base_torch_dist "
+        "--load /root/Qwen3-4B-Base_miles/ "
+        "--save /root/Qwen3-4B-Base_miles/ "
+        "--save-interval 1000 "
+    )
+
+    SFT_ARGS = (
+        "--rollout-function-path miles.rollout.sft_rollout.generate_rollout "
+        "--prompt-data /root/openhermes2_5.parquet "
+        "--input-key messages "
+        "--rollout-shuffle "
+        "--num-epoch 3 "
+        "--rollout-batch-size 128 "
+        "--global-batch-size 128 "
+        "--loss-type sft_loss "
+        "--calculate-per-token-loss "
+        "--disable-compute-advantages-and-returns "
+        "--debug-train-only "
+    )
+
+    PERF_ARGS = (
+        "--tensor-model-parallel-size 1 "
+        "--sequence-parallel "
+        "--pipeline-model-parallel-size 1 "
+        "--context-parallel-size 1 "
+        "--expert-model-parallel-size 1 "
+        "--expert-tensor-parallel-size 1 "
+
+        "--recompute-granularity full "
+        "--recompute-method uniform "
+        "--recompute-num-layers 1 "
+
+        # --micro-batch-size 1
+        "--use-dynamic-batch-size "
+        "--max-tokens-per-gpu 9216 "
+    )
+
+    OPTIMIZER_ARGS = (
+        "--optimizer adam "
+        "--lr 1e-5 "
+        "--lr-warmup-iters 128 "
+        "--lr-decay-style cosine "
+        "--min-lr 1e-6 "
+        "--lr-warmup-fraction 0.9 "
+        "--weight-decay 0.1 "
+        "--adam-beta1 0.9 "
+        "--adam-beta2 0.95 "
+    )
+
+    WANDB_ARGS = (
+        # --use-wandb
+        # --wandb-project miles-dev
+        # --wandb-group qwen3-4B-base-sft
+        # --wandb-key ${WANDB_KEY}
+    )
+
+    MISC_ARGS = (
+        # default dropout in megatron is 0.1
+        "--attention-dropout 0.0 "
+        "--hidden-dropout 0.0 "
+        # should be good for model performance
+        "--accumulate-allreduce-grads-in-fp32 "
+        "--attention-softmax-in-fp32 "
+        # need to comment this when using model with MLA
+        "--attention-backend flash "
+    )
+
     train_args = (
         f"{ckpt_args} "
         f"{rollout_args} "
