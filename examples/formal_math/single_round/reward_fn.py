fd57ebb0ee18c99881b854e0de0acb41f34145af
fzyzcjy <ch271828n@outlook.com>
2025-10-17 10:39:23 +0800

    more

--- examples/formal_math/single_round/reward_fn.py
+++ examples/formal_math/single_round/reward_fn.py
@@ -67,7 +67,7 @@ def _extract_answer_code_from_response_code_block(response_code_block: str):
         return response_code_block[response_code_block.index(haystack) :]
     else:
         # leanabell prover style: only output the proof code
-        return response_code_block
+        return ":= by\n" + response_code_block
 
 
 def _extract_last_full_code_block(text):
