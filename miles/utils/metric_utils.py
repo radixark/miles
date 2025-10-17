59a3edd7cd9a1fadcd864e40339de9c7cbc50c3a
fzyzcjy <ch271828n@outlook.com>
2025-10-17 15:58:29 +0800

    more

--- miles/utils/metric_utils.py
+++ miles/utils/metric_utils.py
@@ -14,29 +14,30 @@ def compute_pass_rate(
     assert len(flat_rewards) == num_groups * group_size
     rewards_of_group = np.array(flat_rewards).reshape(num_groups, group_size)
 
-    def estimate_pass_at_k(num_samples, num_correct, k):
-        """
-        Estimates pass@k of each problem and returns them in an array.
-        """
-
-        def estimator(n, c, k):
-            """
-            Calculates 1 - comb(n - c, k) / comb(n, k).
-            """
-            if n - c < k:
-                return 1.0
-            return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
-
-        return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples, num_correct)])
-
     log_dict = {}
     for k in pass_rate_name_list:
         num_correct = np.sum(rewards_of_group == 1, axis=1)
         num_samples = np.full(num_groups, group_size)
 
-        pass_k_estimates = estimate_pass_at_k(num_samples, num_correct, k)
+        pass_k_estimates = _estimate_pass_at_k(num_samples, num_correct, k)
 
         pass_k = np.mean(pass_k_estimates)
         log_dict[f"pass@{k}"] = pass_k
 
     return log_dict
+
+def _estimate_pass_at_k(num_samples, num_correct, k):
+    """
+    Estimates pass@k of each problem and returns them in an array.
+    """
+
+    def estimator(n, c, k):
+        """
+        Calculates 1 - comb(n - c, k) / comb(n, k).
+        """
+        if n - c < k:
+            return 1.0
+        return 1.0 - np.prod(1.0 - k / np.arange(n - c + 1, n + 1))
+
+    return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples, num_correct)])
+
