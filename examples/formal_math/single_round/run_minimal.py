dcce1f2bb8dc07f185459376f0cbd641d31e6951
fzyzcjy <ch271828n@outlook.com>
2025-10-20 10:56:01 +0800

    more

--- examples/formal_math/single_round/run_minimal.py
+++ examples/formal_math/single_round/run_minimal.py
@@ -14,7 +14,12 @@ MODEL_NAME, MODEL_TYPE = "Qwen3-8B", "qwen3-8B"
 
 NUM_GPUS = 8
 
-ckpt_args = f"--hf-checkpoint /root/models/{MODEL_NAME}/ " f"--ref-load /root/models/{MODEL_NAME}_torch_dist "
+ckpt_args = (
+    f"--hf-checkpoint /root/models/{MODEL_NAME}/ " 
+    f"--ref-load /root/models/{MODEL_NAME}_torch_dist "
+    f"--load /root/{MODEL_NAME}_miles "
+    f"--save /root/{MODEL_NAME}_miles "
+)
 
 rollout_args = (
     "--prompt-data /root/datasets/formal_math_single_round/minimal_demo/flc_train.jsonl "
