5ceecab4e15429be0ff303a67526664f47ae1530
fzyzcjy <ch271828n@outlook.com>
2025-10-24 09:16:31 +0800

    more

--- miles/utils/profile_utils.py
+++ miles/utils/profile_utils.py
@@ -5,7 +5,6 @@ import torch
 
 def attach_oom_dump_memory_history():
     torch.cuda.memory._record_memory_history(
-        # keep 100,000 alloc/free events from before the snapshot
         max_entries=100000,
         # record stack information for the trace events
         # trace_alloc_record_context=True,
@@ -15,6 +14,8 @@ def attach_oom_dump_memory_history():
     def oom_observer(device, alloc, device_alloc, device_free):
         path_dump = f"oom_rank-{torch.distributed.get_rank()}_{args.memory_snapshot_path}"
         print(f"Observe OOM, will dump snapshot to {path_dump}. ({device=} {alloc=} {device_alloc=} {device_free=})")
+
+        # TODO use `_dump_snapshot` instead?
         snapshot = torch.cuda.memory._snapshot()
         dump(snapshot, open(path_dump, "wb"))
 
