418751963e6db21f697ae2974b36c74e25e8e327
fzyzcjy <ch271828n@outlook.com>
2025-11-12 16:10:01 +0800

    more

--- scripts/run_deepseek.py
+++ scripts/run_deepseek.py
@@ -235,8 +235,8 @@ def train(args: ScriptArgs):
     )
 
     sglang_decode_max_bs = 256
-    sglang_world_size = 4 if args.num_nodes <= 2 else 64
-    sglang_attn_dp_size = 1 if args.num_nodes <= 2 else 8
+    sglang_world_size = 4 if args.num_nodes <= 4 else 64
+    sglang_attn_dp_size = 1 if args.num_nodes <= 4 else 8
     sglang_attn_tp_size = sglang_world_size // sglang_attn_dp_size
     sglang_args = (
         f"--rollout-num-gpus-per-engine {sglang_world_size} "
