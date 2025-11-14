040cd392d004474ad3fdee2a8be0de02ddabb8d0
fzyzcjy <ch271828n@outlook.com>
2025-11-14 10:50:17 +0800

    more

--- tests/ci/github_runner/entrypoint.sh
+++ tests/ci/github_runner/entrypoint.sh
@@ -6,11 +6,13 @@ set -eux
 # args ref: https://github.com/actions/runner/blob/68ff57dbc4c836d50f46602a8a53301fb9513eb4/src/Runner.Listener/CommandSettings.cs#L53
 
 DIR_BASE="/data/miles_ci/runner_$(hostname)"
+
+mkdir -p "$DIR_BASE"
+mkdir -p "$DIR_BASE/github_actions_work"
+
 cd $DIR_BASE
 
 CONFIG_EXECUTED_FLAG_PATH="$DIR_BASE/miles_runner_config_executed.txt"
-
-mkdir -p "$DIR_BASE"
 
 if [ ! -f "$CONFIG_EXECUTED_FLAG_PATH" ]; then
   /home/runner/config.sh --url ${GITHUB_RUNNER_URL} --token ${GITHUB_RUNNER_TOKEN} --unattended --work "$DIR_BASE/github_actions_work"
