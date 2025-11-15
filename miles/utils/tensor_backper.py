8bacaa3a6ce143020821d5a0f53a6b5376786411
fzyzcjy <ch271828n@outlook.com>
2025-11-15 11:38:41 +0800

    more

--- miles/utils/tensor_backper.py
+++ miles/utils/tensor_backper.py
@@ -1,4 +1,5 @@
-from typing import Dict, Iterable, Callable, Tuple
+from collections import defaultdict
+from typing import Dict, Iterable, Callable, Tuple, Any
 
 import torch
 
@@ -8,18 +9,21 @@ NamedParametersGetter = Callable[[], Iterable[Tuple[str, torch.Tensor]]]
 class TensorBackuper:
     def __init__(self, source_getter: NamedParametersGetter):
         self._source_getter = source_getter
+        self._backups: Dict[str, Dict[str, torch.Tensor]] = defaultdict()
 
     @torch.no_grad()
     def backup(self, tag: str) -> None:
+        backup_dict = self._backups[tag]
         for name, param in self._source_getter():
-            if name not in params_dict:
-                params_dict[name] = torch.empty_like(param, device=torch.device("cpu"), pin_memory=True)
-            params_dict[name].copy_(param.detach(), non_blocking=True)
+            if name not in backup_dict:
+                backup_dict[name] = torch.empty_like(param, device=torch.device("cpu"), pin_memory=True)
+            backup_dict[name].copy_(param.detach(), non_blocking=True)
         torch.cuda.synchronize()
 
     @torch.no_grad()
     def restore(self, tag: str) -> None:
+        backup_dict = self._backups[tag]
         for name, param in self._source_getter():
-            assert name in params_dict
-            param.copy_(params_dict[name], non_blocking=True)
+            assert name in backup_dict
+            param.copy_(backup_dict[name], non_blocking=True)
         torch.cuda.synchronize()
