436692f45f8be1d84c1280c3eb1fc559e59a7d53
fzyzcjy <ch271828n@outlook.com>
2025-11-17 18:14:55 +0800

    more

--- miles/utils/observability_utils.py
+++ miles/utils/observability_utils.py
@@ -1,5 +1,8 @@
-from miles.utils.wandb_utils import init_wandb_secondary
+from miles.utils.wandb_utils import init_wandb_primary, init_wandb_secondary
 
 
-def init_observability(args, primary: bool = True):
-    init_wandb_secondary(args)
+def init_observability(args, primary: bool = True, **kwargs):
+    if primary:
+        init_wandb_primary(args, **kwargs)
+    else:
+        init_wandb_secondary(args, **kwargs)
