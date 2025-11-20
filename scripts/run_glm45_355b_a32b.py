9d355b7dca7eec8195bbf92ca69506f997e14371
fzyzcjy <ch271828n@outlook.com>
2025-11-20 18:01:10 +0800

    more

--- scripts/run_glm45_355b_a32b.py
+++ scripts/run_glm45_355b_a32b.py
@@ -25,7 +25,8 @@ class ScriptArgs(U.ExecuteTrainConfig):
     extra_args: str = ""
     rollout_fp8: bool = False
     dynamic_sampling: bool = False
-    task: Literal["dapo_aime", "gsm8k"] = "dapo_aime"
+    # TODO use more complex task
+    task: Literal["dapo_aime", "gsm8k"] = "gsm8k"
 
 
 @app.command()
