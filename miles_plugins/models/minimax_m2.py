e5c1db27b6e73a367dcd563a60f1db9be59afba7
fzyzcjy <ch271828n@outlook.com>
2025-11-21 16:23:19 +0800

    more

--- miles_plugins/models/minimax_m2.py
+++ miles_plugins/models/minimax_m2.py
@@ -35,7 +35,6 @@ def get_minimax_m2_spec(args, config, vp_stage):
             ("k_layernorm", args.num_query_groups),
         ]:
             v_old = getattr(self_attention_submodules, k)
-            print(f"_create_per_layer_rms_norm.new {k=} {v_old=} {num_heads=}")
             v_new = _create_per_layer_rms_norm(v_old, num_heads=num_heads)
             setattr(self_attention_submodules, k, v_new)
         transformer_layer_spec.layer_specs[layer_id] = layer_spec
@@ -74,9 +73,8 @@ class _PerLayerRMSNormExtraArgs:
 # ref: WrappedTorchNorm, TENorm
 class _PerLayerRMSNorm:
     def __new__(cls, *args, hidden_size: int, extra: _PerLayerRMSNormExtraArgs, **kwargs):
-        print(f"_PerLayerRMSNorm.new {args=} {hidden_size=} {extra=} {kwargs=}")
         # MiniMax-M2 head_dim, can remove this assertion when more model is to be supported
-        # assert hidden_size == 128
+        assert hidden_size == 128
 
         obj = build_module(extra.inner_cls, *args, hidden_size=hidden_size * extra.num_heads, **kwargs)
 
