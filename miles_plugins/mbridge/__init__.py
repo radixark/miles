d526c7311605373289000655d2a80d2cf5a5a9e5
fzyzcjy <ch271828n@outlook.com>
2025-11-21 11:53:32 +0800

    more

--- miles_plugins/mbridge/__init__.py
+++ miles_plugins/mbridge/__init__.py
@@ -2,5 +2,6 @@ from .glm4 import GLM4Bridge
 from .glm4moe import GLM4MoEBridge
 from .mimo import MimoBridge
 from .qwen3_next import Qwen3NextBridge
+from .minimax_m2 import MinimaxM2Bridge
 
-__all__ = ["GLM4Bridge", "GLM4MoEBridge", "Qwen3NextBridge", "MimoBridge"]
+__all__ = ["GLM4Bridge", "GLM4MoEBridge", "Qwen3NextBridge", "MimoBridge", "MinimaxM2Bridge"]
---
+++ miles_plugins/mbridge/minimax_m2.py
@@ -0,0 +1,7 @@
+from mbridge.core import register_model
+from mbridge.models import Qwen2MoEBridge
+
+
+@register_model("minimax_m2")
+class MinimaxM2Bridge(Qwen2MoEBridge):
+    TODO
