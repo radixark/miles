9c037f5bc50ed74bbe920c5864215e8c02e0b2ff
fzyzcjy <ch271828n@outlook.com>
2025-11-22 16:58:53 +0800

    fix

--- miles/backends/megatron_utils/checkpoint.py
+++ miles/backends/megatron_utils/checkpoint.py
@@ -14,11 +14,14 @@ __all__ = ["save_checkpoint"]
 
 def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, checkpointing_context, skip_load_to_model_and_opt):
     # ref: how megatron `load_checkpoint` gets directory
-    load_path = get_args().load
+    args = get_args()
+    load_path = args.load
 
     if _is_hf_checkpoint(load_path):
         return _load_checkpoint_hf(
             ddp_model=ddp_model,
+            optimizer=optimizer,
+            args=args,
             load_path=load_path,
         )
     else:
@@ -39,7 +42,7 @@ def _is_hf_checkpoint(path: str):
         return False
 
 
-def _load_checkpoint_hf(ddp_model, load_path: str):
+def _load_checkpoint_hf(ddp_model, optimizer, args, load_path: str):
     from megatron.bridge import AutoBridge
 
     logger.info(f"Load checkpoint from HuggingFace model into Megatron (path={load_path})")
@@ -47,6 +50,11 @@ def _load_checkpoint_hf(ddp_model, load_path: str):
 
     with megatron_bridge_utils.patch_megatron_model(ddp_model):
         bridge.load_hf_weights(ddp_model)
+
+    # Copied from Megatron-core :: load_checkpoint (with simplifications)
+    if (args.fp16 or args.bf16) and optimizer is not None:
+        assert not args.load_main_params_from_ckpt
+        optimizer.reload_model_params()
 
     # We can see `successfully loaded checkpoint from ... [ t 1/2, p 1/1 ] at iteration 0`
     # when loading Megatron, thus it is 0
