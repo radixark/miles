e8f819fab617274f0753c47fcdd3e8e37b4fa0a7
fzyzcjy <ch271828n@outlook.com>
2025-11-22 11:00:58 +0800

    more

--- miles/utils/megatron_bridge_utils.py
+++ miles/utils/megatron_bridge_utils.py
@@ -1,6 +1,14 @@
 from contextlib import contextmanager
+from megatron.core.utils import unwrap_model
 
 
 @contextmanager
-def patch_megatron_model():
-    yield
+def patch_megatron_model(model):
+    model_config = unwrap_model(model)[0].config
+    assert not hasattr(model_config, "share_embeddings_and_output_weights")
+    setattr(model_config, "share_embeddings_and_output_weights", TODO)
+
+    try:
+        yield
+    finally:
+        delattr(model_config, "share_embeddings_and_output_weights")
