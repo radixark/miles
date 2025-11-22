3a078005c39649ac28762578a27738c85ae4df3c
fzyzcjy <ch271828n@outlook.com>
2025-11-22 08:55:53 +0800

    more

--- miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
+++ miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
@@ -4,3 +4,6 @@ from .hf_weight_iterator_base import HfWeightIteratorBase
 class HfWeightIteratorDirect(HfWeightIteratorBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def get_hf_weight_chunks(self, megatron_local_weights):
+        TODO
