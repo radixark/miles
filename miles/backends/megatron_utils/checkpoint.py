6500060f492e190267507f22e89d3731bbbab6d2
fzyzcjy <ch271828n@outlook.com>
2025-11-22 08:37:30 +0800

    more

--- miles/backends/megatron_utils/checkpoint.py
+++ miles/backends/megatron_utils/checkpoint.py
@@ -3,16 +3,24 @@ from megatron.training.checkpointing import save_checkpoint
 from megatron.training.checkpointing import load_checkpoint as _load_checkpoint_megatron
 
 
-def load_checkpoint():
-    if _is_hf_checkpoint(TODO):
-        return _load_checkpoint_hf()
-    else:
-        return _load_checkpoint_megatron()
+def load_checkpoint(ddp_model, optimizer, opt_param_scheduler, checkpointing_context, skip_load_to_model_and_opt):
+    fn = (
+        _load_checkpoint_hf
+        if _is_hf_checkpoint(TODO)
+        else _load_checkpoint_megatron
+    )
+    return fn(
+        ddp_model=ddp_model,
+        optimizer=optimizer,
+        opt_param_scheduler=opt_param_scheduler,
+        checkpointing_context=checkpointing_context,
+        skip_load_to_model_and_opt=skip_load_to_model_and_opt,
+    )
 
 
 def _is_hf_checkpoint():
     return TODO
 
 
-def _load_checkpoint_hf():
+def _load_checkpoint_hf(ddp_model, optimizer, opt_param_scheduler, checkpointing_context, skip_load_to_model_and_opt):
     return TODO
