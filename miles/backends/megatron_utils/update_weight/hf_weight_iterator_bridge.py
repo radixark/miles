d63fe8183ec4dc95f419d56e08529e70a280af21
fzyzcjy <ch271828n@outlook.com>
2025-11-22 09:02:16 +0800

    more

--- miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
+++ miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
@@ -5,5 +5,10 @@ class HfWeightIteratorBridge(HfWeightIteratorBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        from megatron.bridge import AutoBridge
+
+        self._bridge = AutoBridge.from_hf_pretrained(TODO_what)
+
     def get_hf_weight_chunks(self, megatron_local_weights):
-        TODO
+        vanilla_named_weights = self._bridge.export_hf_weights(self.model, cpu=False)
+        yield from _chunk(vanilla_named_weights)
