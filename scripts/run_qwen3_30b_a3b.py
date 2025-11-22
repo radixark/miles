19f96abf656a69e93e9568ac97909a917ba30db4
fzyzcjy <ch271828n@outlook.com>
2025-11-22 08:53:30 +0800

    more

--- scripts/run_qwen3_30b_a3b.py
+++ scripts/run_qwen3_30b_a3b.py
@@ -122,7 +122,7 @@ def execute(args: ScriptArgs):
     )
 
     if args.enable_megatron_bridge:
-        misc_args += "--megatron-to-hf-via-bridge "
+        misc_args += "--megatron-to-hf-mode bridge "
 
     match (args.hardware, args.num_nodes):
         case ("H100", 1):
