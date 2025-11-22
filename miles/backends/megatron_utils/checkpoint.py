a9cff0d1511e7124e0d1cd331ebac97636a9b0d8
fzyzcjy <ch271828n@outlook.com>
2025-11-22 08:34:50 +0800

    more

--- miles/backends/megatron_utils/checkpoint.py
+++ miles/backends/megatron_utils/checkpoint.py
@@ -1,4 +1,14 @@
 # TODO: may need to copy those 2 functions and do refactoring.
-from megatron.training.checkpointing import load_checkpoint, save_checkpoint
+from megatron.training.checkpointing import save_checkpoint
+from megatron.training.checkpointing import load_checkpoint as _load_checkpoint_megatron
 
-__all__ = ["load_checkpoint", "save_checkpoint"]
+
+def load_checkpoint():
+    if _is_hf_checkpoint(TODO):
+        return _load_checkpoint_hf()
+    else:
+        return _load_checkpoint_megatron()
+
+
+def _load_checkpoint_hf():
+    return TODO
