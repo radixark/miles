4ac6bd02a18ddec8c9b29422908d2284e9c14cd1
fzyzcjy <ch271828n@outlook.com>
2025-11-22 09:14:25 +0800

    more

--- miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
+++ miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
@@ -19,30 +19,3 @@ class HfWeightIteratorBridge(HfWeightIteratorBase):
         vanilla_named_weights = self._bridge.export_hf_weights(self.model, cpu=False, conversion_tasks=self._conversion_tasks)
         yield from chunk_named_params_by_size(vanilla_named_weights, chunk_size=self.args.update_weight_buffer_size)
 
-
-# TODO fsdp can also use this
-def chunk_named_params_by_size(named_params: Iterable[Tuple[str, torch.Tensor]], chunk_size: int):
-    return _chunk_by_size(
-        named_params,
-        compute_size=lambda named_weight: named_weight[1].nbytes,
-        chunk_size=chunk_size,
-    )
-
-
-def _chunk_by_size(objects: Iterable[Any], compute_size: Callable[[Any], int], chunk_size: int):
-    bucket: List[Any] = []
-    bucket_size = 0
-
-    for obj in objects:
-        obj_size = compute_size(obj)
-
-        if bucket and (bucket_size + obj_size) >= chunk_size:
-            yield bucket
-            bucket = []
-            bucket_size = 0
-
-        bucket.append(obj)
-        bucket_size += obj_size
-
-    if bucket:
-        yield bucket
