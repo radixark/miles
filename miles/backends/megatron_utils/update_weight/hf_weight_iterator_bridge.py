84ff92fc93b2780372dbfc57e811cd2d9cc61032
fzyzcjy <ch271828n@outlook.com>
2025-11-22 09:27:35 +0800

    more

--- miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
+++ miles/backends/megatron_utils/update_weight/hf_weight_iterator_bridge.py
@@ -10,8 +10,8 @@ class HfWeightIteratorBridge(HfWeightIteratorBase):
 
         from megatron.bridge import AutoBridge
 
-        self._bridge = AutoBridge.from_hf_pretrained(TODO_what)
-        self._vanilla_conversion_tasks = self._bridge.build_conversion_tasks(TODO_hf_config, self.model)
+        self._bridge = AutoBridge.from_hf_pretrained(self.args.hf_checkpoint)
+        self._vanilla_conversion_tasks = self._bridge.build_conversion_tasks(self._bridge.hf_pretrained, self.model)
 
     def get_hf_weight_chunks(self, megatron_local_weights):
         # TODO support quantization (e.g. modify megatron-bridge to provide megatron param name)
