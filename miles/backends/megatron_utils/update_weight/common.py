ac8fa6d8a16b16d0100e4f03da3259ae62846621
fzyzcjy <ch271828n@outlook.com>
2025-11-23 12:00:05 +0800

    more

--- miles/backends/megatron_utils/update_weight/common.py
+++ miles/backends/megatron_utils/update_weight/common.py
@@ -123,17 +123,17 @@ def named_params_and_buffers(
 
 def _named_params_and_buffers_vanilla(model: Sequence[torch.nn.Module]) -> Iterator[tuple[str, torch.Tensor]]:
     for vp_stage, model_module in enumerate(model):
-        def compute_fqn(name):
+        def _compute_fqn(name):
             return f"vp_stages.{vp_stage}.{name}"
 
         for name, param in model_module.named_parameters():
-            yield compute_fqn(name), param
+            yield _compute_fqn(name), param
 
         for name, buffer in model_module.named_buffers():
             # TODO shall we handle (almost) all buffers like Megatron Bridge
             if "expert_bias" not in name:
                 continue
-            yield compute_fqn(name), buffer
+            yield _compute_fqn(name), buffer
 
 
 def _named_params_and_buffers_global(
