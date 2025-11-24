d307359e818d2fc225db024ffc3889b0e6b2ce66
fzyzcjy <ch271828n@outlook.com>
2025-11-24 08:42:37 +0800

    more

--- miles_plugins/megatron_bridge/minimax_m2_bridge.py
+++ miles_plugins/megatron_bridge/minimax_m2_bridge.py
@@ -2,7 +2,6 @@ import logging
 
 import torch
 from megatron.core.models.gpt.gpt_model import GPTModel
-from transformers import MinimaxM2ForCausalLM
 
 from megatron.bridge.models.conversion.mapping_registry import MegatronMappingRegistry
 from megatron.bridge.models.conversion.model_bridge import MegatronModelBridge
@@ -11,13 +10,12 @@ from megatron.bridge.models.conversion.param_mapping import (
     GatedMLPMapping,
     QKVMapping,
 )
-from megatron.bridge.models.glm.glm45_provider import GLMMoEModelProvider
 from megatron.bridge.models.hf_pretrained.causal_lm import PreTrainedCausalLM
 
 logger = logging.getLogger(__name__)
 
 
-@MegatronModelBridge.register_bridge(source=MinimaxM2ForCausalLM, target=GPTModel)
+@MegatronModelBridge.register_bridge(source="MinimaxM2ForCausalLM", target=GPTModel)
 class MinimaxM2Bridge(MegatronModelBridge):
     def build_conversion_tasks(self, hf_pretrained, megatron_model):
         self._hf_config = hf_pretrained.config
