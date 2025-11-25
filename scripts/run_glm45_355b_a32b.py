629ffa80d08e12ab842e74cb1b59a9b1a2869742
fzyzcjy <ch271828n@outlook.com>
2025-11-25 13:38:18 +0800

    more

--- scripts/run_glm45_355b_a32b.py
+++ scripts/run_glm45_355b_a32b.py
@@ -91,8 +91,8 @@ def _prepare_cp(args: ScriptArgs):
     )
     if args.rollout_fp8:
         U.rsync_simple(
-            path_src=f"/root/models/{args.model_name}_FP8",
-            path_dst=f"/root/local_data/{args.model_name}_FP8",
+            path_src=f"/root/models/{args.model_name}-FP8",
+            path_dst=f"/root/local_data/{args.model_name}-FP8",
         )
 
 
@@ -103,7 +103,7 @@ def train(args: ScriptArgs):
     _prepare_cp(args)
 
     hf_checkpoint = (
-        f"/root/local_data/{args.model_name}_FP8" if args.rollout_fp8 else f"/root/local_data/{args.model_name}"
+        f"/root/local_data/{args.model_name}-FP8" if args.rollout_fp8 else f"/root/local_data/{args.model_name}"
     )
 
     load_save_path = f"/root/shared_data/{args.run_id}/checkpoints"
