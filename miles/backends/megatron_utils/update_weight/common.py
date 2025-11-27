ac994a28b459edeec83be9d98ed735174d6eea8f
fzyzcjy <ch271828n@outlook.com>
2025-11-27 19:58:47 +0800

    more

--- miles/backends/megatron_utils/update_weight/common.py
+++ miles/backends/megatron_utils/update_weight/common.py
@@ -125,11 +125,18 @@ def named_params_and_buffers(
         ans = _named_params_and_buffers_vanilla(model)
 
     if translate_gpu_to_cpu:
-        from torch_memory_saver import torch_memory_saver
-
-        ans = ((name, torch_memory_saver.get_cpu_backup(tensor) or tensor) for name, tensor in ans)
+        ans = ((name, _maybe_get_cpu_backup(tensor)) for name, tensor in ans)
 
     return ans
+
+
+def _maybe_get_cpu_backup(x: torch.Tensor):
+    from torch_memory_saver import torch_memory_saver
+
+    if (cpu_tensor := torch_memory_saver.get_cpu_backup(x)) is not None:
+        return cpu_tensor
+
+    return x
 
 
 def _named_params_and_buffers_vanilla(model: Sequence[torch.nn.Module]) -> Iterator[tuple[str, torch.Tensor]]:
