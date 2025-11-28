e5e1ddddd0723733c462868f87cc58c01b76b9c6
fzyzcjy <ch271828n@outlook.com>
2025-11-28 20:35:37 +0800

    more

--- miles/utils/typer_utils.py
+++ miles/utils/typer_utils.py
@@ -1,8 +1,10 @@
+import typer
 import dataclasses
 import inspect
+from typing import Annotated
 
 
-def dataclass_cli(func):
+def dataclass_cli(func, env_var_prefix: str = "MILES_SCRIPT_"):
     """Modified from https://github.com/fastapi/typer/issues/154#issuecomment-1544876144"""
 
     # The dataclass type is the first argument of the function.
@@ -14,16 +16,26 @@ def dataclass_cli(func):
     # To construct the signature, we remove the first argument (self)
     # from the dataclass __init__ signature.
     signature = inspect.signature(dataclass_cls.__init__)
-    parameters = list(signature.parameters.values())
-    if len(parameters) > 0 and parameters[0].name == "self":
-        del parameters[0]
+    old_parameters = list(signature.parameters.values())
+    if len(old_parameters) > 0 and old_parameters[0].name == "self":
+        del old_parameters[0]
+
+    new_parameters = []
+    for param in old_parameters:
+        env_var_name = f"{env_var_prefix}{param.name.upper()}"
+        new_annotation = Annotated[param.annotation, typer.Option(envvar=env_var_name)]
+        new_parameters.append(param.replace(annotation=new_annotation))
 
     def wrapped(**kwargs):
         return func(dataclass_cls(**kwargs))
 
-    wrapped.__signature__ = signature.replace(parameters=parameters)
+    wrapped.__signature__ = signature.replace(parameters=new_parameters)
     wrapped.__doc__ = func.__doc__
     wrapped.__name__ = func.__name__
     wrapped.__qualname__ = func.__qualname__
 
     return wrapped
+
+# unit test
+if __name__ == '__main__':
+    TODO
