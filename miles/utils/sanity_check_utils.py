7b830cacd30736539f0d0cb60c7b5d457728c8e5
fzyzcjy <ch271828n@outlook.com>
2025-12-02 15:46:03 +0800

    more

--- miles/utils/sanity_check_utils.py
+++ miles/utils/sanity_check_utils.py
@@ -16,21 +16,24 @@ class WeightChangeChecker:
 
     def step(self):
         curr_state = _snapshot(self.weights_getter())
-        assert set(initial_state.keys()) == set(final_state.keys())
-
-        all_tensor_names = list(initial_state)
-        unchanged_tensor_names = [name for name in all_tensor_names if initial_state[name] == final_state[name]]
-        changed_tensor_names = sorted(list(set(all_tensor_names) - set(unchanged_tensor_names)))
-
-        # TODO
-        # TODO
-        # TODO
-        # assert (
-        #     len(unchanged_tensor_names) == 0
-        # ), f"{unchanged_tensor_names=} {changed_tensor_names=} {initial_state=} {final_state=}"
-        # logger.info(f"WeightChangeChecker passed ({len(unchanged_tensor_names)=}, {len(changed_tensor_names)=})")
-        print(f"hi {unchanged_tensor_names=} {changed_tensor_names=} {initial_state=} {final_state=}")
 
 
 def _snapshot(weights_dict: dict[str, torch.Tensor]):
     return {name: compute_hash_tensor(param) for name, param in weights_dict.items()}
+
+
+def _check(state_a: dict[str, int], state_b: dict[str, int]):
+    assert set(state_a.keys()) == set(state_b.keys())
+    all_tensor_names = list(state_a)
+
+    unchanged_tensor_names = [name for name in all_tensor_names if state_a[name] == state_b[name]]
+    changed_tensor_names = sorted(list(set(all_tensor_names) - set(unchanged_tensor_names)))
+
+    # TODO
+    # TODO
+    # TODO
+    # assert (
+    #     len(unchanged_tensor_names) == 0
+    # ), f"{unchanged_tensor_names=} {changed_tensor_names=} {state_a=} {state_b=}"
+    # logger.info(f"WeightChangeChecker passed ({len(unchanged_tensor_names)=}, {len(changed_tensor_names)=})")
+    print(f"hi {unchanged_tensor_names=} {changed_tensor_names=} {state_a=} {state_b=}")
