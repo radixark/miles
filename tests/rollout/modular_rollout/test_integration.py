29a310dee78f7e194a2cf1ff4bd6d8c577745489
fzyzcjy <ch271828n@outlook.com>
2026-01-14 20:40:22 +0800

    more

--- tests/rollout/modular_rollout/test_integration.py
+++ tests/rollout/modular_rollout/test_integration.py
@@ -37,7 +37,11 @@ def _expected_sample(*, group_index: int | None) -> Sample:
 
 
 _ROLLOUT_ARGV_VARIANTS = [
-    pytest.param([], id="old_rollout_old_generate"),
+    pytest.param([
+        "--rollout-function-path", "miles.rollout.sglang_rollout.generate_rollout",
+        "--eval-rollout-function-path", "miles.rollout.sglang_rollout.generate_rollout",
+        "--custom-generate-function-path", "miles.rollout.sglang_rollout.generate",
+    ], id="old_rollout_old_generate"),
     pytest.param(
         [
             "--rollout-function-path", "miles.rollout.modular_rollout.orchestration_train.SimpleTrainRolloutFn",
@@ -49,6 +53,7 @@ _ROLLOUT_ARGV_VARIANTS = [
     pytest.param([
         "--rollout-function-path", "miles.rollout.modular_rollout.orchestration_train.SimpleTrainRolloutFn",
         "--eval-rollout-function-path", "miles.rollout.modular_rollout.orchestration_eval.SimpleEvalRolloutFn",
+        "--custom-generate-function-path", "miles.rollout.modular_rollout.inference_wrapper.generate",
     ], id="new_rollout_new_generate"),
 ]
 
