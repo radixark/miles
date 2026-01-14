4eae03bf820debfc72a16d01f830ec08a88b5dbc
fzyzcjy <ch271828n@outlook.com>
2026-01-14 21:15:49 +0800

    fmt

--- miles/rollout/modular_rollout/orchestration_common.py
+++ miles/rollout/modular_rollout/orchestration_common.py
@@ -6,7 +6,7 @@ from typing import Any
 
 import numpy as np
 
-from miles.rollout.base_types import GenerateFnInput, GenerateFnOutput
+from miles.rollout.base_types import GenerateFnInput
 from miles.rollout.modular_rollout.compatibility import load_generate_function
 from miles.rollout.modular_rollout.inference_wrapper import generate
 from miles.rollout.rm_hub import async_rm, batched_async_rm
