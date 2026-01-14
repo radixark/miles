b2772d7ef2e74dd7b6a1314e99f3113ae23c0510
fzyzcjy <ch271828n@outlook.com>
2026-01-14 16:00:06 +0800

    more

--- miles/rollout/modular_rollout/compatibility.py
+++ miles/rollout/modular_rollout/compatibility.py
@@ -1,6 +1,18 @@
-from miles.rollout.base_types import RolloutFnInput, RolloutFnOutput
+from miles.rollout.base_types import RolloutFnInput, RolloutFnOutput, RolloutFnConstructorInput, RolloutFnTrainOutput, \
+    RolloutFnEvalOutput
 
 
 class LegacyRolloutFnAdapter:
+    def __init__(self, input: RolloutFnConstructorInput):
+        self.args = input.args
+        self.data_source = input.data_source
+        self.fn = TODO
+
     def __call__(self, input: RolloutFnInput) -> RolloutFnOutput:
-        TODO
+        output = self.fn(self.args, input.rollout_id, self.data_source, evaluation=input.evaluation)
+
+        # compatibility for legacy version
+        if not isinstance(output, (RolloutFnTrainOutput, RolloutFnEvalOutput)):
+            output = RolloutFnEvalOutput(data=output) if input.evaluation else RolloutFnTrainOutput(samples=output)
+
+        return output
