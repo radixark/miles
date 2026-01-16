c2fb508104dcdf084bef8f2b87830a1f7e3f9c23
fzyzcjy <ch271828n@outlook.com>
2026-01-16 17:04:14 +0800

    fmt

--- miles/rollout/generate_hub/multi_turn_single_sample.py
+++ miles/rollout/generate_hub/multi_turn_single_sample.py
@@ -11,7 +11,6 @@ from sglang.srt.entrypoints.openai.protocol import Tool
 from sglang.srt.function_call.function_call_parser import FunctionCallParser
 
 from miles.rollout.base_types import GenerateFnInput, GenerateFnOutput
-from miles.rollout.generate_hub.generate_endpoint_wrapper import _get_rollout_routed_experts_from_response
 from miles.rollout.generate_hub.tool_call_utils import tokenize_tool_responses
 from miles.utils.http_utils import post
 from miles.utils.misc import load_function
