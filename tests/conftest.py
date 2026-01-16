225db28a5dfe6237ee51c228ce3e4549b50d0630
fzyzcjy <ch271828n@outlook.com>
2026-01-16 10:16:38 +0800

    more

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from tests.fixtures.rollout_integration import rollout_integration_env
 from tests.fixtures.generation_fixtures import generation_env
+from tests.fixtures.rollout_integration import rollout_integration_env
 
 _ = rollout_integration_env, generation_env
