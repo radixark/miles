1ba88e1a43bbe6f47f1d573dd671099a1e72e18d
fzyzcjy <ch271828n@outlook.com>
2026-01-17 09:38:17 +0800

    more

--- miles/rollout/generate_hub/agentic_tool_call.py
+++ miles/rollout/generate_hub/agentic_tool_call.py
@@ -21,57 +21,7 @@ from miles.utils.misc import load_function
 
 
 async def generate(input: GenerateFnInput) -> GenerateFnOutput:
-    # ----------------------- Setup -------------------------
-
-    args = input.args
-    sample = input.sample
-    tokenizer = input.state.tokenizer
-    assert not args.partial_rollout, "Partial rollout is not supported"
-
-    url = f"http://{args.sglang_router_ip}:{args.sglang_router_port}/generate"
-
-    execute_tool_function = load_function(args.generate_execute_tool_function_path)
-
-    tool_specs = load_function(args.generate_tool_specs_path)
-    tool_call_parser = create_tool_call_parser(tool_specs, args.generate_tool_call_parser)
-
-    extra_samples = []
-
-    # ----------------------- Initial prompts -------------------------
-
-    prompt_tokens_ids = compute_prompt_ids_from_sample(input.state, sample, tools=tool_specs)
-
-    sample.loss_mask = []
-    sample.tokens = prompt_tokens_ids.copy()
-
-    for turn in range(args.generate_max_turns):
-        # ----------------------- Call inference endpoint -------------------------
-
-        payload, halt_status = compute_request_payload(args, sample.tokens, input.sampling_params)
-        if payload is None:
-            sample.status = halt_status
-            break
-
-        # Bookkeeping only for multi-sample mode
-        if args.generate_multi_samples and turn > 0:
-            extra_samples.append(deepcopy(sample))
-
-        output = await post(url, payload)
-        await update_sample_from_response(args, sample, payload=payload, output=output, update_loss_mask=True)
-
-        if output["meta_info"]["finish_reason"]["type"] in ("abort", "length"):
-            break
-
-        # ----------------------- Execute tools -------------------------
-
-        _, tool_calls = tool_call_parser.parse_non_stream(output["text"])
-        if len(tool_calls) == 0:
-            break
-
-        tool_messages = await execute_tool_calls(tool_calls, execute_tool_function)
-        update_sample_with_tool_responses(sample, tool_messages, tokenizer=tokenizer)
-
-    return GenerateFnOutput(samples=(extra_samples + [sample]) if args.generate_multi_samples else sample)
+    TODO
 
 
 def _add_arguments(parser: argparse.ArgumentParser):
@@ -87,4 +37,55 @@ generate.add_arguments = _add_arguments
 
 class _ToolCallAgent:
     """Imagine this is a black-box agent that does arbitrarily complex work."""
-    TODO
+    async def run(self):
+        # ----------------------- Setup -------------------------
+
+        args = input.args
+        sample = input.sample
+        tokenizer = input.state.tokenizer
+        assert not args.partial_rollout, "Partial rollout is not supported"
+
+        url = f"http://{args.sglang_router_ip}:{args.sglang_router_port}/generate"
+
+        execute_tool_function = load_function(args.generate_execute_tool_function_path)
+
+        tool_specs = load_function(args.generate_tool_specs_path)
+        tool_call_parser = create_tool_call_parser(tool_specs, args.generate_tool_call_parser)
+
+        extra_samples = []
+
+        # ----------------------- Initial prompts -------------------------
+
+        prompt_tokens_ids = compute_prompt_ids_from_sample(input.state, sample, tools=tool_specs)
+
+        sample.loss_mask = []
+        sample.tokens = prompt_tokens_ids.copy()
+
+        for turn in range(args.generate_max_turns):
+            # ----------------------- Call inference endpoint -------------------------
+
+            payload, halt_status = compute_request_payload(args, sample.tokens, input.sampling_params)
+            if payload is None:
+                sample.status = halt_status
+                break
+
+            # Bookkeeping only for multi-sample mode
+            if args.generate_multi_samples and turn > 0:
+                extra_samples.append(deepcopy(sample))
+
+            output = await post(url, payload)
+            await update_sample_from_response(args, sample, payload=payload, output=output, update_loss_mask=True)
+
+            if output["meta_info"]["finish_reason"]["type"] in ("abort", "length"):
+                break
+
+            # ----------------------- Execute tools -------------------------
+
+            _, tool_calls = tool_call_parser.parse_non_stream(output["text"])
+            if len(tool_calls) == 0:
+                break
+
+            tool_messages = await execute_tool_calls(tool_calls, execute_tool_function)
+            update_sample_with_tool_responses(sample, tool_messages, tokenizer=tokenizer)
+
+        return GenerateFnOutput(samples=(extra_samples + [sample]) if args.generate_multi_samples else sample)
