e6514d7651f2df6777fd88305d6c4ef66479566c
fzyzcjy <ch271828n@outlook.com>
2026-01-17 22:24:29 +0800

    more

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ _ = rollout_integration_env, generation_env
 
 @pytest.fixture(autouse=True)
 def enable_experimental_rollout_refactor():
-    """自动为所有测试启用实验性 rollout refactor"""
     os.environ["MILES_EXPERIMENTAL_ROLLOUT_REFACTOR"] = "1"
     yield
     os.environ.pop("MILES_EXPERIMENTAL_ROLLOUT_REFACTOR", None)
