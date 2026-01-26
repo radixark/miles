e177d6ac0f7bc5cd8cd1a3242b62e4bca0a67143
fzyzcjy <ch271828n@outlook.com>
2026-01-26 22:58:50 +0800

    quant

--- miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_fp8.py
+++ miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_fp8.py
@@ -75,6 +75,11 @@ def quantize_params_fp8(args, megatron_name, converted_named_params, quantizatio
         "self_attention.linear_q_up_proj.weight",
         "self_attention.linear_kv_down_proj.weight",
         "self_attention.linear_kv_up_proj.weight",
+        # deepseek v4 attention (wo_a uses quant_config=None, so keep bf16)
+        "self_attention.wq_a.weight",
+        "self_attention.wq_b.weight",
+        "self_attention.wkv.weight",
+        "self_attention.wo_b.weight",
         # dsa indexer
         "self_attention.core_attention.indexer.linear_wq_b.weight",
         "self_attention.core_attention.indexer.linear_wk.weight",
