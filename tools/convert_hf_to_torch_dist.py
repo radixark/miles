3b06b75a06817abc28779630e74cb3e8ef94be53
Jiajun Li <guapisolo@gmail.com>
2026-02-02 23:07:52 +0000

    Fix mcore ckpt conversion on AMD gpus

--- tools/convert_hf_to_torch_dist.py
+++ tools/convert_hf_to_torch_dist.py
@@ -1,7 +1,6 @@
 import gc
 import os
 import shutil
-from functools import wraps
 
 import torch
 import torch.distributed as dist
@@ -12,30 +11,11 @@ from megatron.training.training import get_model
 
 import miles_plugins.mbridge  # noqa: F401
 from mbridge import AutoBridge
-from mbridge.core.bridge import Bridge
 from miles.backends.megatron_utils.arguments import set_default_megatron_args
 from miles.backends.megatron_utils.initialize import init
 from miles.backends.megatron_utils.model_provider import get_model_provider_func
 from miles.utils.logging_utils import configure_logger
 from miles.utils.memory_utils import print_memory
-
-
-def patch_weight_to_mcore_format_preserve_fp32():
-
-    original_method = Bridge._weight_to_mcore_format
-
-    @wraps(original_method)
-    def patched_method(self, mcore_weights_name, hf_weights):
-        original_dtype = getattr(self, "dtype", None)
-        self.dtype = None
-        try:
-            result = original_method(self, mcore_weights_name, hf_weights)
-        finally:
-            self.dtype = original_dtype
-        return result
-
-    Bridge._weight_to_mcore_format = patched_method
-    print("[Patch] Applied patch to preserve FP32 precision in _weight_to_mcore_format")
 
 
 def add_convertion_args(parser):
@@ -126,17 +106,21 @@ def main():
     )
     args = get_args()
     init(args)
+
+    # if using AMD gpus, we have to do the conversion in cpu
+    if hasattr(torch.version, "hip") and torch.version.hip is not None:
+        assert args.use_cpu_initialization, "AMD GPU requires --use_cpu_initialization=True"
+
     model = get_model(get_model_provider_func(args), ModelType.encoder_or_decoder, wrap_with_ddp=False)
 
     # Load model
     hf_model_path = args.hf_checkpoint
     bridge = AutoBridge.from_pretrained(hf_model_path, trust_remote_code=True)
-
-    # Patch to preserve FP32 precision for _keep_fp32 params
-    patch_weight_to_mcore_format_preserve_fp32()
-
     bridge.load_weights(model, hf_model_path, memory_efficient=True)
     print(f"Model loaded: {hf_model_path}")
+
+    if args.use_cpu_initialization:
+        model[0] = model[0].cpu()
 
     print_memory("after loading model")
     torch.cuda.synchronize()
