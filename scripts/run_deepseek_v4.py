e9e5f6053312547369be51da8549682606a537c4
Yueming Yuan <yym022502@gmail.com>
2026-02-05 14:51:35 -0800

    add node 7 config

--- scripts/run_deepseek_v4.py
+++ scripts/run_deepseek_v4.py
@@ -214,6 +214,16 @@ def train(args: ScriptArgs):
             "--expert-model-parallel-size 8 "
             "--expert-tensor-parallel-size 1 "
         )
+    elif args.num_nodes <= 7:
+        perf_args = (
+            "--tensor-model-parallel-size 8 "
+            "--sequence-parallel "
+            "--pipeline-model-parallel-size 7 "
+            "--pipeline-model-parallel-layout 'E,t*4\\|t*7\\|t*7\\|t*7\\|t*7\\|t*7\\|t*4,L' "
+            "--context-parallel-size 1 "
+            "--expert-model-parallel-size 8 "
+            "--expert-tensor-parallel-size 1 "
+        )
     elif args.num_nodes <= 8:
         perf_args = (
             "--tensor-model-parallel-size 8 "
