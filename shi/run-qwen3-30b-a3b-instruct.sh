bc74af5fc485bfc75108b162dc481909797c10fe
Shi Dong <hello@dongshi.me>
2026-02-24 22:21:13 -0800

    script update

--- shi/run-qwen3-30b-a3b-instruct.sh
+++ shi/run-qwen3-30b-a3b-instruct.sh
@@ -153,7 +153,7 @@ echo "  SWE Agent URL: ${SWE_AGENT_GYM_URL}"
 
 ray job submit --address="http://127.0.0.1:8265" \
     --runtime-env-json="${RUNTIME_ENV_JSON}" \
-    -- python3 train.py \
+    -- python3 ${HOME}/miles/train.py \
     --actor-num-nodes 1 \
     --actor-num-gpus-per-node 8 \
     --colocate \
