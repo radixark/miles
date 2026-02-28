0481b722cfa2b2eaac2f8f9b8096e4c3a3822ea9
yueming-yuan <yym022502@gmail.com>
2026-02-27 16:50:05 -0800

    add indexer weight in mxfp8 quantizer

--- miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_mxfp8.py
+++ miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_mxfp8.py
@@ -68,6 +68,8 @@ def quantize_params_mxfp8(args, megatron_name, converted_named_params, quantizat
         "self_attention.linear_q_up_proj.weight",
         "self_attention.linear_kv_down_proj.weight",
         "self_attention.linear_kv_up_proj.weight",
+        "self_attention.wq_b.weight",
+        "self_attention.wk.weight",
     ]:
         quantize_named_params = []
         for converted_name, param in converted_named_params:
