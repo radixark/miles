9cb704468e867d12b4a381a0807ef795330b4346
fzyzcjy <ch271828n@outlook.com>
2026-03-05 14:52:20 +0800

    refactor: deduplicate step extraction in tracking_utils.log()

--- miles/utils/tracking_utils.py
+++ miles/utils/tracking_utils.py
@@ -28,11 +28,12 @@ def log(args, metrics, step_key: str):
     if args.use_wandb:
         wandb.log(metrics)
 
-    if args.use_tensorboard:
-        metrics_except_step = {k: v for k, v in metrics.items() if k != step_key}
-        _TensorboardAdapter(args).log(data=metrics_except_step, step=metrics[step_key])
-
-    if _ft_tracking_agent is not None:
-        step_value = metrics.get(step_key, 0)
+    if args.use_tensorboard or _ft_tracking_agent is not None:
+        step_value = int(metrics.get(step_key, 0))
         metrics_without_step = {k: v for k, v in metrics.items() if k != step_key}
-        _ft_tracking_agent.log(metrics=metrics_without_step, step=int(step_value))
+
+        if args.use_tensorboard:
+            _TensorboardAdapter(args).log(data=metrics_without_step, step=step_value)
+
+        if _ft_tracking_agent is not None:
+            _ft_tracking_agent.log(metrics=metrics_without_step, step=step_value)
