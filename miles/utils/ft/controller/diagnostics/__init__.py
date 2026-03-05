2ff723f88a14872df135982653f1c77f91558a3e
fzyzcjy <ch271828n@outlook.com>
2026-03-05 08:47:57 +0800

    feat(diagnostics): export InterMachineCommDiagnostic from __init__

--- miles/utils/ft/controller/diagnostics/__init__.py
+++ miles/utils/ft/controller/diagnostics/__init__.py
@@ -1,5 +1,11 @@
 from miles.utils.ft.controller.diagnostics.base import BaseDiagnostic
+from miles.utils.ft.controller.diagnostics.inter_machine_comm import InterMachineCommDiagnostic
 from miles.utils.ft.controller.diagnostics.intra_machine_comm import IntraMachineCommDiagnostic
 from miles.utils.ft.controller.diagnostics.scheduler import DiagnosticScheduler
 
-__all__ = ["BaseDiagnostic", "IntraMachineCommDiagnostic", "DiagnosticScheduler"]
+__all__ = [
+    "BaseDiagnostic",
+    "InterMachineCommDiagnostic",
+    "IntraMachineCommDiagnostic",
+    "DiagnosticScheduler",
+]
