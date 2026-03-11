66178040fbd648011fd96f61a7223f8d38959b8d
fzyzcjy <ch271828n@outlook.com>
2026-03-11 12:21:33 +0800

    add TrainingSubsystemActuator
    Implements SubsystemActuatorProtocol for training subsystem.
    stop/start raise NotImplementedError (Level 1 restart not yet supported).
    get_status delegates to MainJobProtocol.
    Part of M4: subsystem type definitions.

---
+++ miles/utils/ft/adapters/impl/training_actuator.py
@@ -0,0 +1,25 @@
+from miles.utils.ft.adapters.types import JobStatus, MainJobProtocol, SubsystemActuatorProtocol
+
+
+class TrainingSubsystemActuator(SubsystemActuatorProtocol):
+    """Training has no Level 1 restart capability yet.
+
+    The sub-SM does not call stop()/start(); instead it transitions to
+    RestartingMainJob state to request a job-level restart.
+    """
+
+    def __init__(self, main_job: MainJobProtocol) -> None:
+        self._main_job = main_job
+
+    async def stop(self) -> None:
+        raise NotImplementedError(
+            "Training subsystem-level restart not yet supported; use RestartingMainJob"
+        )
+
+    async def start(self) -> str:
+        raise NotImplementedError(
+            "Training subsystem-level restart not yet supported; use RestartingMainJob"
+        )
+
+    async def get_status(self) -> JobStatus:
+        return await self._main_job.get_job_status()
