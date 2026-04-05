b030c9ecc50a2d8d100fd20d952c1dee96dc08e7
yueming-yuan <yym022502@gmail.com>
2026-04-05 16:42:13 -0700

    Remove unnecessary hasattr check for sglang_router_policy

--- miles/ray/rollout.py
+++ miles/ray/rollout.py
@@ -941,7 +941,7 @@ def _start_router(args, *, has_pd_disaggregation: bool = False, force_new: bool 
         router_args.log_level = "warn"
         router_args.request_timeout_secs = args.sglang_router_request_timeout_secs
 
-        if hasattr(args, "sglang_router_policy") and args.sglang_router_policy:
+        if args.sglang_router_policy:
             router_args.policy = args.sglang_router_policy
 
         if has_pd_disaggregation:
