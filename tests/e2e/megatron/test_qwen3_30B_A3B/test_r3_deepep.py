e00bcc2e0721d72d4a02761241179adf47f07a7f
Jiajun Li <guapisolo@gmail.com>
2026-05-20 01:11:10 +0000

    fix fp8 ci and add TODO fp8 qat r3 ci

--- tests/e2e/megatron/test_qwen3_30B_A3B/test_r3_deepep.py
+++ tests/e2e/megatron/test_qwen3_30B_A3B/test_r3_deepep.py
@@ -3,16 +3,11 @@ import os
 from tests.ci.ci_register import register_cuda_ci
 from tests.e2e.megatron.test_qwen3_30B_A3B._common import CaseConfig, execute, prepare
 
-register_cuda_ci(
-    est_time=1800,
-    suite="stage-c-4-gpu-h200",
-    labels=["megatron"],
-    disabled="Mismatch between fp8 rollout and bf16 training. Need to add QAT later.",
-)
+register_cuda_ci(est_time=1800, suite="stage-c-4-gpu-h200", labels=["megatron"])
 
 CASE = CaseConfig(
     use_deepep=True,
-    use_fp8_rollout=True,
+    use_fp8_rollout=False,
     use_int4_rollout=False,
     use_bridge=False,
     use_r3=True,
