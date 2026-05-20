aa2b4ed2005a2376b59077855bca54549ef8f680
Jiajun Li <guapisolo@gmail.com>
2026-05-20 08:17:04 +0000

    fix parallel 0.6; wandb and r3 still buggy

--- tests/e2e/precision/test_qwen3_0.6B_parallel_check.py
+++ tests/e2e/precision/test_qwen3_0.6B_parallel_check.py
@@ -5,7 +5,7 @@ from tests.ci.ci_register import register_cuda_ci
 import miles.utils.external_utils.command_utils as U
 
 register_cuda_ci(
-    est_time=1200,
+    est_time=2400,
     suite="stage-c-8-gpu-h100",
     labels=["megatron", "precision"],
 )
