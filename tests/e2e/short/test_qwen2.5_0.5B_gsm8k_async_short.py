acb1dc9cd365e1b58b4034edf7ba73944de948a3
Jiajun Li <guapisolo@gmail.com>
2026-05-21 02:37:55 +0000

    revert back to 8 gpus

--- tests/e2e/short/test_qwen2.5_0.5B_gsm8k_async_short.py
+++ tests/e2e/short/test_qwen2.5_0.5B_gsm8k_async_short.py
@@ -4,11 +4,13 @@ from tests.ci.ci_register import register_cuda_ci
 
 import miles.utils.external_utils.command_utils as U
 
-register_cuda_ci(est_time=240, suite="stage-c-2-gpu-h200", labels=["short"])
+register_cuda_ci(est_time=240, suite="stage-c-8-gpu-h100", labels=["short"])
+
+FEW_GPU = U.get_bool_env_var("MILES_TEST_FEW_GPU", "0")
 
 MODEL_NAME = "Qwen2.5-0.5B-Instruct"
 MODEL_TYPE = "qwen2.5-0.5B"
-NUM_GPUS = 2
+NUM_GPUS = 4 if FEW_GPU else 8
 
 
 def prepare():
@@ -93,8 +95,8 @@ def execute():
         "--attention-softmax-in-fp32 "
         "--attention-backend flash "
         "--actor-num-nodes 1 "
-        f"--actor-num-gpus-per-node {max(1, NUM_GPUS // 4)} "
-        f"--rollout-num-gpus {NUM_GPUS * 3 // 4} "
+        f"--actor-num-gpus-per-node {1 if FEW_GPU else 2} "
+        f"--rollout-num-gpus {3 if FEW_GPU else 6} "
         "--megatron-to-hf-mode bridge "
     )
 
