6c631b142c2329bb6a40d4123087448102a3f255
Yueming Yuan <yym022502@gmail.com>
2026-06-01 15:41:56 -0700

    Add TODO for improvement in weight block size handling
    Added a TODO comment to address the hacky implementation for [128, 128] weight block size.

--- miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_fp8.py
+++ miles/backends/megatron_utils/megatron_to_hf/processors/quantizer_fp8.py
@@ -104,6 +104,7 @@ def _quantize_param(args, name, weight, weight_block_size):
         if _get_scale_format(args, name, weight_block_size) == "ue8m0":
             qweight, scale = quant_weight_ue8m0(weight, weight_block_size=weight_block_size)
             scale = transform_scale_ue8m0(scale, mn=qweight.shape[-2])
+        # TODO: this [128, 128] is hacky. need improve
         elif per_block_cast_to_fp8 is not None and list(weight_block_size) == [128, 128]:
             qweight, scale = per_block_cast_to_fp8(weight)
         else:
