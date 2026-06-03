98244bd21ecd5f6d7ac1aa5d8d584be10cd3e51e
Zhiyao Jiang <jessicajiang324@gmail.com>
2026-06-03 21:55:28 +0000

    daily docker build with merged dockerfile for rocm

--- docker/build.py
+++ docker/build.py
@@ -53,6 +53,7 @@ VARIANTS = {
         "dockerfile": "docker/Dockerfile.rocm",
         "build_args": {
             "GPU_ARCH": "gfx950",
+            "SGLANG_IMAGE_TAG": "v0.5.10-rocm720-mi35x",
         },
     },
 }
