d325d525f93405b87071d9888ac1b509a0b7fda2
Mathew Han <mathewjhan@gmail.com>
2026-06-25 14:24:08 -0700

    [fix] don't require xor custom_rm_path/rm_type

--- miles/utils/adapter_config.py
+++ miles/utils/adapter_config.py
@@ -56,11 +56,6 @@ class AdapterConfig:
 
     metadata: dict[str, Any] = field(default_factory=dict)
 
-    def __post_init__(self):
-        if bool(self.rm_type) == bool(self.custom_rm_path):
-            raise ValueError("Only one of rm_type or custom_rm_path should be set in AdapterConfig")
-
-
 @dataclass(frozen=True)
 class RegisteredAdapter:
     """Join view of an adapter's static config and current lifetime state.
