73261dccd3db579579f775215c6d6b07ab46456f
Mathew Han <mathewjhan@gmail.com>
2026-07-05 20:57:52 -0700

    [misc] remove torch dist conversion from provision

--- examples/multi_lora/provision.sh
+++ examples/multi_lora/provision.sh
@@ -10,11 +10,3 @@ hf download --repo-type dataset zhuzilin/dapo-math-17k \
 # Download training dataset (gsm8k)
 hf download --repo-type dataset zhuzilin/gsm8k \
   --local-dir /root/gsm8k
-
-# Convert to dist
-source scripts/models/qwen3-4B.sh
-
-PYTHONPATH=/root/Megatron-LM python tools/convert_hf_to_torch_dist.py \
-    ${MODEL_ARGS[@]} \
-    --hf-checkpoint /root/Qwen3-4B \
-    --save /root/Qwen3-4B_dist
