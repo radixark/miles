083f10f34ed1a74a9ec71642509237ad058ddc57
fzyzcjy <ch271828n@outlook.com>
2026-08-02 20:29:51 +0800

    impl

--- miles/utils/workers/naming.py
+++ miles/utils/workers/naming.py
@@ -1,2 +1,9 @@
+# TODO refactor & move later
 def compute_worker_name(*, spec_name: str, cell_index: int = 0, worker_in_cell_index: int = 0) -> str:
     return f"{spec_name}-{cell_index}-{worker_in_cell_index}"
+
+
+# TODO refactor & move later
+def parse_worker_name(worker_name: str) -> tuple[str, int, int]:
+    spec_name, cell_index, worker_in_cell_index = worker_name.rsplit("-", maxsplit=2)
+    return spec_name, int(cell_index), int(worker_in_cell_index)
