5838e6b43fd2b625fb43c3da7e6e46e6d18f4f23
yueming-yuan <yym022502@gmail.com>
2026-09-09 16:05:09 -0700

    Align gateway tests with datum names and simplified APIs

--- tests/fast/tinker/test_loss_tables.py
+++ tests/fast/tinker/test_loss_tables.py
@@ -2,7 +2,7 @@
 
 from miles.backends.training_utils.loss_hub.tinker_losses import TINKER_LOSS_FUNCTIONS
 from miles.tinker.core.types import LOSS_FN_INPUTS, LOSS_INPUT_KEYS
-from miles.tinker.runtime import ROW_TO_BATCH_KEYS
+from miles.tinker.runtime import DATUM_TO_BATCH_KEYS
 
 
 def test_every_registered_loss_has_an_admission_entry():
@@ -10,4 +10,4 @@ def test_every_registered_loss_has_an_admission_entry():
 
 
 def test_wire_keys_map_onto_trainer_batch_keys():
-    assert set(LOSS_INPUT_KEYS.values()) == set(ROW_TO_BATCH_KEYS)
+    assert set(LOSS_INPUT_KEYS.values()) == set(DATUM_TO_BATCH_KEYS)
