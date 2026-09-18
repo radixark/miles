0ea2aa228e8846d42a00ce86f010cb364122746a
Tom <ch271828n@outlook.com>
2026-09-18 15:26:33 +0800

    Resolve rollout function paths in place before hook registration
    Pass rollout normalization through a modify_args callback while keeping it in arguments.py.

--- miles/utils/args/custom_function.py
+++ miles/utils/args/custom_function.py
@@ -1,21 +1,24 @@
 import argparse
+from collections.abc import Callable
 
-from miles.utils.arguments import resolve_rollout_function_paths
 from miles.utils.environ import use_legacy_rollout_v1
 from miles.utils.function_registry import load_function
 from miles.utils.workers.argv_utils import with_relax_parser_required_args, with_suppressed_parser_help
 
 
-def add_user_provided_function_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+def add_user_provided_function_arguments(
+    parser: argparse.ArgumentParser, *, modify_args: Callable[[argparse.Namespace], None]
+) -> argparse.ArgumentParser:
     try:
         with with_relax_parser_required_args(parser), with_suppressed_parser_help(parser):
             args_partial, _ = parser.parse_known_args()
     except SystemExit:
         return parser
+    modify_args(args_partial)
     paths = [args_partial.custom_inference_engine_provider_path]
     if not use_legacy_rollout_v1():
         paths = [
-            resolve_rollout_function_paths(args_partial)[0],
+            args_partial.rollout_function_path,
             args_partial.custom_generate_function_path,
             *paths,
         ]
